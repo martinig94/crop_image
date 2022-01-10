b009f483e96cea1bcb37d7f8b9ebbb7dfecc1d53
MARTINI Giulia <martini.giulia94@gmail.com>
2022-01-10 21:28:47 +0100

    Feat: Add download url

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author="Giulia Martini",  # Type in your name
     author_email="martini.giulia94@gmail.com",  # Type in your E-Mail
     url="https://github.com/martinig94",  # Provide either the link to your github or to your website
-    download_url="https://github.com/user/reponame/archive/v_01.tar.gz",  # I explain this later on
+    download_url="https://github.com/martinig94/crop_image/archive/refs/tags/v0.1-alpha.tar.gz",
     keywords=["crop", "picture", "image"],  # Keywords that define your package best
-    install_requires=["Pillow"],  # I get to this in a second
+    install_requires=["Pillow"],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
